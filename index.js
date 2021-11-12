a173b6b55b46b7f2e3151a78eed0d55746a7814c
Jose  Prieto <92605951+AtlasNavigator@users.noreply.github.com>
2021-11-12 21:19:20 +0000

    More exmaples.

--- index.js
+++ index.js
@@ -4,6 +4,20 @@
 //   console.log(i); //Same while loop, but using for, so it's pretty much in one line.
 // }
 
-for (let i=0; i <= 1000; i++){
-  console.log(i); //Count to 1000 using for
-}
+// for (let i=0; i <= 1000; i++){
+//   console.log(i); //Count to 1000 using for
+// }
+
+// for (let i = 100; i>=12; i--){
+//   console.log(i); //Count down to 12
+// }
+
+// for (let i = 100; i<=300; i++) {
+//   console.log(i); //Count from 100 to 300
+// }
+
+// for (let i=10; i<=100; i++) {
+//   if (i%2 ==0){
+//     console.log (i); //Anything mod 2 from 10-100 without a remainder is printed
+//   }
+// }
