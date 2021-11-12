3c5ce97ab457b736133275d29c160e93a99edb14
Jose  Prieto <92605951+AtlasNavigator@users.noreply.github.com>
2021-11-12 21:14:23 +0000

    First 2 examples

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 // For Loops Notes
 
+// for (let i=1; i<10; i++) {
+//   console.log(i); //Same while loop, but using for, so it's pretty much in one line.
+// }
+
+for (let i=0; i <= 1000; i++){
+  console.log(i); //Count to 1000 using for
+}
